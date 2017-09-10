bcbd70002822049b6b17ea7d13722082ab76834c
khachuy2705 <huy.2705@gmail.com>
2017-09-10 14:43:15 +0700

    Cập nhật code: 14:43:15.20, Sun 09/10/2017

--- Break_capcha_VNP_v1.py
+++ Break_capcha_VNP_v1.py
@@ -1,17 +1,23 @@
 import os
 import requests
+
+#Cài đặt PIL bằng pip3 install Pillow
 from PIL import Image
+import base64
 from base64 import decodestring
 #Cấu hình chung
-
+capcha_path='D:\Lab_Zone\Python\Break_capcha_VNP\capcha_saved\\'
 #Link nguồn capcha VNP
 URL="http://naptien.vinaphone.com.vn/Home/GenerateCaptcha?_=1504926074759"
 
+#Hàm lấy ảnh về từ VNP
 def save_capcha(so_luong):
-	r = requests.get(URL)
-	
-	image = Image.fromstring('RGB',(width,height),decodestring(r.content))
-	image.save("foo.png")
-	a=
-	return a
-print(save_capcha(1))
+	for i in range(1,so_luong):
+		r = requests.get(URL)
+		png_recovered = base64.decodestring(r.content)
+		f = open(capcha_path+"_"+str(i)+"_"+".png", "wb")
+		f.write(png_recovered)
+		f.close()
+	return "OK"
+
+# print(save_capcha(1000))
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Nhận diện capcha trên site naptien.vinaphone.com.vn ứng dụng machine le
 ## Giai đoạn 1 - Chuẩn bị dữ liệu
 1. Chuẩn bị thư viện ảnh mẫu:
 	Ảnh được get từ URL: http://naptien.vinaphone.com.vn/Home/GenerateCaptcha?_=1504926074759
+	Load 1000 ảnh mẫu về: Đã load xong
 2. Xử lý nhiễu
 3. Cắt ảnh thành từng ký tự
 
